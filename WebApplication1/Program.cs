de522c2cc672a864b1e860fe543bf032df638b1a
Quyen Ly <quyen.ly2@dxc.com>
2024-11-07 12:03:47 +0700

    jwt

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using WebApplication1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +12,65 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // Add JWT Bearer authorization to Swagger
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Type = SecuritySchemeType.Http,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
 builder.Services.AddDbContext<QLBHDbContext>(options =>
 options.UseSqlServer("Data Source=.;Initial Catalog=QL_BANHANG;User ID=user_QLBH;Trust Server Certificate=True;Password=1234"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "your_issuer", // replace with your issuer
+        ValidAudience = "your_audience", // replace with your audience
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key_your_secret_key_your_secret_key_your_secret_keyyour_secret_key_your_secret_key_your_secret_key_your_secret_key")) // replace with your secret key
+    };
+});
+
+// Add services to the container
+builder.Services.AddSingleton<JwtHelper>(new JwtHelper(
+    secretKey: "your_secret_key_your_secret_key_your_secret_key_your_secret_keyyour_secret_key_your_secret_key_your_secret_key_your_secret_key",  // Use a strong secret key
+    issuer: "your_issuer",         // Define the issuer
+    audience: "your_audience"      // Define the audience
+));
+
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
